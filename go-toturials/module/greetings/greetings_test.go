4d3d1fb0eccb97f5409569d71ef6c77509e6c4fe
daniel-yang1739 <ytyang6690@gmail.com>
2026-01-08 15:07:24 +0800

    module

---
+++ go-toturials/module/greetings/greetings_test.go
@@ -0,0 +1,25 @@
+package greetings
+
+
+import (
+	"testing"
+	"regexp"
+)
+
+
+func TestHelloName(t *testing.T) {
+	name := "Daniel"
+	want := regexp.MustCompile(`\b`+name+`\b`)
+	msg, err := Hello("Daniel")
+	if !want.MatchString(msg) || err != nil {
+		t.Errorf(`Hello("Daniel") = %q, %v, want match for %#q, nil`, msg, err, want)
+	}
+}
+
+
+func TestHelloEmpty(t *testing.T) {
+	msg, err := Hello("")
+	if msg != "" || err == nil {
+		t.Errorf(`Hello("") = %q, %v, want "", error`, msg, err)
+	}
+}
